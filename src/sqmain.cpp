f871167a89321e970a8b43c9d53d0e65f233cb69
AdeonMaster <adeonmaster@gmail.com>
2024-07-25 09:27:18 +0300

    Initial commit

--- src/sqmain.cpp
+++ src/sqmain.cpp
@@ -4,31 +4,15 @@
 
 using namespace Gothic_II_Addon;
 
-/* squirreldoc (func)
-*
-* This is is an example hook for oCGame::Render method.
-*
-* @version	0.1
-* @side		client
-* @name		oCGame__Render
-* @param	(oCGame*) self the oCGame ptr to object instance.
-* @param	(void*) vtable the vftable param.
-*
-*/
-void __fastcall oCGame__Render(oCGame* self, void* vtable);
-auto Hook_oCGame_Render = Union::CreateHook((void*)0x006C86A0, oCGame__Render, Union::HookType::Hook_Detours);
-void __fastcall oCGame__Render(oCGame* self, void* vtable)
+void __fastcall zCAICamera__AI_LookingCam(zCAICamera* self, void* vtable);
+auto Hook_zCAICamera_AI_LookingCam = Union::CreateHook((void*)0x004A3690, zCAICamera__AI_LookingCam, Union::HookType::Hook_Detours);
+void __fastcall zCAICamera__AI_LookingCam(zCAICamera* self, void* vtable)
 {
-	// g2o uses dynamic hooks (once that revert patch to original value & invoke the original function), tso we need to follow this pattern here
-	Hook_oCGame_Render.Disable();
+	Hook_zCAICamera_AI_LookingCam.Disable();
 
-	printf("render call\n");
+	self->AI_LookingCam();
 
-	// always invoke original method! (calling overloaded union call operator on Hook_oCGame_Render will result in a crash)
-	self->Render();
-
-	// if we want to keep our hook active, we need to reapply it
-	Hook_oCGame_Render.Enable();
+	Hook_zCAICamera_AI_LookingCam.Enable();
 }
 
 extern "C" SQRESULT SQRAT_API sqmodule_load(HSQUIRRELVM vm, HSQAPI api)
